acdc76644732ebb1b1a120a800c98c9c701c9bf8
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-04-22 01:18:30 +0500

    Урок 4: ДЗ со звездочкой  - Реализовать ViewComponent для брендов

---
+++ WebStore/Models/BrandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Base.Interface;
+
+namespace WebStore.Models
+{
+    public class BrandViewModel : INamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        /// <summary>
+        /// Отращает кол-во товаров на данный момент у этого брэнда
+        /// </summary>
+        public int CountProducts { get; set; }
+    }
+}
---
+++ WebStore/ViewComponents/BrandsViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.ViewComponents
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductService _productService;
+        public BrandsViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var brands = GetBrands();
+            return View(brands);
+        }
+
+        private List<BrandViewModel> GetBrands()
+        {
+            var brands = _productService.GetBrands();
+            var query = _productService.GetProducts(new Domain.ProductFilter())
+                .GroupBy(x => x.BrandId);
+            // получим и заполним родительские категории
+            //var parentSections = brands.Where(p => !p.ParentId.HasValue).ToArray();
+            var result = new List<BrandViewModel>();
+            foreach (var curProduct in query)
+            {
+                var curBrand = brands.FirstOrDefault(x => x.Id == curProduct.Key);
+                if (curBrand == null) continue;
+                result.Add(new BrandViewModel()
+                {
+                    Id = curBrand.Id,
+                    Name = curBrand.Name,
+                    Order = curBrand.Order,
+                    CountProducts = curProduct.Count()
+                });
+            }
+            return result;
+        }
+    }
+}
