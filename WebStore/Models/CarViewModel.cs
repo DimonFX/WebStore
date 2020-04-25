3a0acda26e543ea6d1e9766c98bec1e8d6df52b5
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-04-25 22:18:13 +0500

    Использовал атрибуты в CarViewModel, что бы изабиться от кода в представлении CarView и использовать Html.EditorForModel()

--- WebStore/Models/CarViewModel.cs
+++ WebStore/Models/CarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,26 +8,23 @@ namespace WebStore.Models
 {
     public class CarViewModel
     {
+        [ScaffoldColumn(false)]
         public int Id { get; set; }
+        [Display(Name ="Марка")]
         public string Mark { get; set; }
+        [Display(Name = "Модель")]
         public string CarModel { get; set; }
-        /// <summary>
-        /// Тип кузова
-        /// </summary>
+        [Display(Name = "Тип кузова")]
         public string BodyType { get; set; }
-        /// <summary>
-        /// год выпуска
-        /// </summary>
+        [Display(Name = "Год выпуска")]
         public int YearRelease { get; set; }
+        [Display(Name = "Трансмиссия")]
         public string Transmission { get; set; }
+        [Display(Name = "Цвет")]
         public string Color { get; set; }
-        /// <summary>
-        /// Привод: задний, полный, передний
-        /// </summary>
+        [Display(Name = "Привод")]
         public string Drive { get; set; }
-        /// <summary>
-        /// Объем двигателя
-        /// </summary>
+        [Display(Name = "Объем двигателя")]
         public double EngineVolume { get; set; }
     }
 }
