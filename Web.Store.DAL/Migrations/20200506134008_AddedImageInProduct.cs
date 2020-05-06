3325027361fe093b23c5eb4aef54777f2b33821b
DimonFX <50489975+DimonFX@users.noreply.github.com>
2020-05-06 18:41:59 +0500

    Урок 8. Добавил Image к Product

---
+++ Web.Store.DAL/Migrations/20200506134008_AddedImageInProduct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web.Store.DAL.Migrations
+{
+    public partial class AddedImageInProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+        }
+    }
+}
